ab3d4c05ce5887a08efb74b8379148391426560c
Idris Somoye <143529985+isomoye-msu@users.noreply.github.com>
2025-01-29 21:34:21 -0500

    Update even_detector.v

--- even_detector.v
+++ even_detector.v
@@ -10,11 +10,11 @@ module even_detector (
   always @(*)
   begin
 
-    even <= (p1 or p2) | (p3 or p4);
-    p1 <= (not a(2)) & (not a(1)) & (not a(0));
-    p2 <= (not a(2)) & a(1) & a(0);
-    p3 <= a(2) & (not a(1)) & a(0);
-    p4 <= a(2) & a(1) & (not a(0));
+    even = (p1 | p2) | (p3 | p4);
+    p1  = (! a[2]) & (! a[1]) & (! a[0]);
+    p2  = (! a[2]) & a[1] & a[0];
+    p3  = a[2] & (! a[1]) & a[0];
+    p4  = a[2] & a[1] & (! a[0]);
 
   end
 
